717c227079d162ec771d0390e5807110f07da972
Testelin Geoffrey <geoffrey.testelin@gmail.com>
2017-11-18 12:35:29 +0100

    Changed jshint latedef rule (not revelant in angular). Fixed error in $httpProvider with dot-notation rule. Removed useless keyboard provider.

--- generators/app/templates/app/config/providers/httpProvider.config.js
+++ generators/app/templates/app/config/providers/httpProvider.config.js
@@ -20,7 +20,7 @@
 
 	function provider($httpProvider) {
 		delete $httpProvider.defaults.headers.common['X-Requested-With'];
-		$httpProvider.defaults.headers.common['Accept']       = 'application/json';
+		$httpProvider.defaults.headers.common.Accept          = 'application/json';
 		$httpProvider.defaults.headers.common['Content-Type'] = 'application/json';
 	}
 
