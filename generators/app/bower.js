5cbf77bf0220f7457273a51a51b73cb16cf994cc
Testelin Geoffrey <geoffrey.testelin@gmail.com>
2017-11-18 16:10:11 +0100

    Changed - The method install in index.js now call npm and bower private methods - The colors are now imported from colors module - The npm modules are now imported from npm module - The bower modules are now imported from bower module

---
+++ generators/app/bower.js
@@ -0,0 +1,46 @@
+/**
+ * Generated header by C0ZEN for generator-altran-angular project
+ * Generated file bower on WebStorm
+ *
+ * Created by: Geoffrey "C0ZEN" Testelin
+ * Date: 18/11/2017
+ * Time: 16:08
+ * Version: 1.0.0
+ */
+(function () {
+	'use strict';
+
+	const bowerModules = [
+		'reset-css',
+		'jquery',
+		'angular',
+		'angular-aria',
+		'angular-animate',
+		'angular-cookies',
+		'angular-messages',
+		'angular-resource',
+		'angular-route',
+		'angular-sanitize',
+		'angular-touch',
+		'moment',
+		'angular-translate',
+		'angular-uuid-service',
+		'angular-logex',
+		'angular-ui-router',
+		'angular-translate-loader-static-files',
+		'angular-translate-loader-url',
+		'angular-translate-loader-partial',
+		'chance',
+		'angular-dynamic-locale',
+		'lodash',
+		'animate.css',
+		'angular-local-storage'
+	];
+
+	module.exports = class Bower {
+		static getModules() {
+			return bowerModules;
+		}
+	};
+
+})();
