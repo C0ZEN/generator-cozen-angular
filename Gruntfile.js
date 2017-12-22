9194c6ffd08dab67206687c91cd19c697925ac72
TESTELIN Geoffrey <geoffrey.testelin@altran.com>
2017-12-22 16:13:04 +0100

    save

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,20 +5,17 @@ module.exports = function (grunt) {
 	const path = require('path');
 
 	const config = {
-		version: require('./package.json').version
+		version: grunt.file.readJSON('package.json').version
 	};
-
-	const staticMappings = {};
 
 	require('time-grunt')(grunt);
 
-	require('jit-grunt')(grunt, staticMappings);
+	require('jit-grunt')(grunt);
 
 	require('load-grunt-config')(grunt, {
 		configPath: path.join(process.cwd(), 'grunt/config'),
 		jitGrunt  : {
-			customTasksDir: 'grunt/tasks',
-			staticMappings
+			customTasksDir: 'grunt/tasks'
 		},
 		data      : {
 			config,
